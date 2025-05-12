ba4fba8842aa7cfce4f1edac60b29cebed0de291
gxickx <mgrokoe@gmail.com>
2025-05-12 09:57:32 -0300

    Cree el commit del tp 3

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+import modelo.Perro;
+
+import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Hola bienvenidos al perro programador\n");
+
+        // modelo.Perro perrito = new modelo.Perro ("Perrito", 20, 0, 5);
+
+        // LISTA DE PERROS
+        ArrayList<Perro> perros = new ArrayList<>();
+
+        perros.add(new Perro("Perrito", 20, 20, 15));
+        perros.add(new Perro("Pipi", 123, 45, 105));
+        perros.add(new Perro("Chloe", 26, 87, 38));
+        perros.add(new Perro("Java", 100, 0, 0));
+
+        //bucle for-each, la variable p representa al perro actual. Instancia individual del objeto modelo.Perro
+        System.out.println("\nEstado Inicial de los perros");
+        for (Perro p : perros) {
+
+            System.out.println(p);
+        }
+        System.out.println("\nPerrros juegan y comen");
+        //cambiar los atributos dentro de la lista (realizando sus métodos)
+        for (Perro p : perros) {
+
+            p.jugar();
+            p.comer();
+
+        }
+
+        // Mostramos el estado final de todos los perros:
+        System.out.println("\nEstado Final de los perros");
+        for (Perro p : perros) {
+
+            System.out.println(p);
+
+        }
+
+
+    }
+}
